114439c1dce654ecb01b5fa42ea9a9ef037683bd
Carlo <carlo.dev2@gmail.com>
2021-03-06 20:38:09 +1100

    added components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import React, { useReducer, useRef } from "react";
+import "./App.css";
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+const App = () => {
+
   return (
-    <Router>
-      <div>
-        NavBar
-      </div>
-    </Router>
-  )
-}
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
 export default App;
