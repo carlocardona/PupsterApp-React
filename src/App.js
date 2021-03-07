062a808977dfcb4ad8bb7b4f18ac65374cb816b7
Carlo <carlo.dev2@gmail.com>
2021-03-07 13:27:58 +1100

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import "./App.css";
-import Navbar from './components/Navbar/Navbar';
+import React from "react"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import About from './components/About/About'
+import Discover from './components/Discover/Discover'
+import Search from './components/Search/Search'
+import "./App.css"
 
 const App = () => {
 
   return (
-    <div>
-      <Navbar />
-    </div>
+      <div className='main'>
+        <div>
+          <Router>
+            <Link to="/">Puppster</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Discover">Discover</Link>
+            <Link to="/Search">Search</Link>
+
+            <Switch>
+              <Route exact path="/About" component={About} />
+              <Route exact path="/Discover" component={Discover} />
+              <Route exact path="/Search" component={Search} />
+            </Switch>
+          </Router>
+        </div>
+      </div>
   );
 };
 
